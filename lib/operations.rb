c198000e02cb2ae4f1f910c44fa743205a90a8dd
bruce willis <fashun_vyctum@yahoo.com>
2020-03-27 00:52:17 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
+if speed > 60 || speed < 40
+  return true
+elsif  speed > 40 && speed < 60
+  return false
+end
 
 end
 
 
 
 def not_safe?(speed)
-	
+speed > 60 || speed <40 ? true : false
+
+
+
+
+	  
 end
 	
-
 
